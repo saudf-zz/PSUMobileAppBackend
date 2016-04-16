8d32b96c78ae65c2bef07df3ce3f1cfde3bca555
saudf <Saud.al.saud@windowslive.com>
2016-04-16 13:12:48 +0300

    minor changes to make front-end work, more enhancements soon

--- PSUMobileAppBackend/login.php
+++ PSUMobileAppBackend/login.php
@@ -1,6 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: http://localhost:4400");
+header('Content-Type: application/json');
 include('db.php');
-    if($_POST['UserID']!= null){
+if(isset($_POST['UserID'])&&isset($_POST['UserPass'])){
         $query = $database->query('SELECT COUNT(*) AS res FROM Students_info WHERE Student_ID ='. addslashes($_POST['UserID'])." AND WEB_PASSWORD='".md5($_POST['UserPass'])."'");
         echo json_encode(mysqli_fetch_array($query));
     }else{
