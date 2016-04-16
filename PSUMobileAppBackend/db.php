19e0009101397ddd7009191cd896930e59f2aaa6
saudf <Saud.al.saud@windowslive.com>
2016-04-16 15:40:34 +0300

    headers unified in a single function, index changed to recieve ID in post then return data, still needs a lot of work though

--- PSUMobileAppBackend/db.php
+++ PSUMobileAppBackend/db.php
@@ -7,4 +7,10 @@ if(!$database->connection){
     echo "Connection error :(";
     break;
 }
+
+function headers(){
+    //should be moved later to a general functions file, this function purpose is unifying headers to make any future changes easy.
+    header("Access-Control-Allow-Origin: http://localhost:4400");
+    header('Content-Type: application/json');
+}
 ?>
--- PSUMobileAppBackend/login.php
+++ PSUMobileAppBackend/login.php
@@ -1,11 +1,11 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:4400");
-header('Content-Type: application/json');
 include('db.php');
+headers();
 if(isset($_POST['UserID'])&&isset($_POST['UserPass'])){
         $query = $database->query('SELECT COUNT(*) AS res FROM Students_info WHERE Student_ID ='. addslashes($_POST['UserID'])." AND WEB_PASSWORD='".md5($_POST['UserPass'])."'");
         echo json_encode(mysqli_fetch_array($query));
     }else{
+    //not validated input
         echo json_encode(array("res"=>-1));
     }
 ?>
